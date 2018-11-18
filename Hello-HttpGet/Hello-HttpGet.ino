7a860617f3d27205e18a6759601050b1004fca23
KazuyukiEguchi <kazuyuki.eguchi@gmail.com>
2018-11-18 15:18:13 +0900

    Update Hello-HttpGet.ino

--- Hello-HttpGet/Hello-HttpGet.ino
+++ Hello-HttpGet/Hello-HttpGet.ino
@@ -6,13 +6,12 @@
 #define USERNAME          "sora"
 #define PASSWORD          "sora"
 
-// #define WEBURL       "https://www.google.com/"
-// #define WEBURL       "http://www.kddi.com/"
-// #define WEBURL       "http://106.162.242.107"
-#define WEBURL       "http://beam.soracom.io:8888/"
+#define WEBURL       "http://httpbin.org/ip"
+// #define WEBURL       "http://beam.soracom.io:8888/"
 
+#define INTERVAL          (60000)
 
-#define INTERVAL          (30000)
+boolean setup_flag = false;
 
 WioCellular Wio;
 
@@ -27,34 +26,52 @@ void setup() {
   
   SerialUSB.println("### Power supply ON.");
   Wio.PowerSupplyCellular(true);
-  delay(500);
+  delay(1000);
 
   SerialUSB.println("### Turn on or reset.");
   if (!Wio.TurnOnOrReset()) {
     SerialUSB.println("### ERROR! ###");
     return;
   }
+  
+  #ifdef ARDUINO_WIO_LTE_M1NB1_BG96
+    SerialUSB.println("### SetSelectNetwork ###");
+    Wio.SetSelectNetwork(WioCellular::SELECT_NETWORK_MODE_MANUAL_IMSI);
+  #endif
 
   SerialUSB.println("### Connecting to \"" APN "\".");
-  Wio.SetSelectNetwork(WioCellular::SELECT_NETWORK_MODE_MANUAL_IMSI);
+  
   if (!Wio.Activate(APN, USERNAME, PASSWORD)) {
-    SerialUSB.println("### ERROR! ###");
+    SerialUSB.println("### APN SET ERROR! ###");
     return;
   }
 
+  setup_flag = true;
 }
 
 void loop() {
-  char data[100];
+  if(setup_flag == false)
+  {
+    delay(5000);
+    setup();
+    return;
+  }
+  
+  char data[3000];  // レスポンスを格納できるだけの容量が必要！
   int status;
 
   SerialUSB.println("GET " WEBURL);
-  status = Wio.HttpGet(WEBURL, data, 100);
+  status = Wio.HttpGet(WEBURL, data, sizeof(data));
   
   SerialUSB.print("RecvBytes:");
   SerialUSB.println(status);
 
-err:
+  if(status > 0)
+  {
+    data[status] = 0x0;
+    SerialUSB.println(data);
+  }
+
   SerialUSB.println("### Wait.");
   delay(INTERVAL);
 }
