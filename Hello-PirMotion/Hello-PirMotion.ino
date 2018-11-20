2d5fa96ad0f58ffea0d090aaec3f7fd1d821fd75
oh-yamada <oh-yamada@kddi.com>
2018-11-20 19:28:02 +0900

    Create Hello-PirMotion.ino

---
+++ Hello-PirMotion/Hello-PirMotion.ino
@@ -0,0 +1,33 @@
+// GROVE PIRモーションセンサを繋いだ場合のサンプルプログラム
+
+#include <WioCellLibforArduino.h>
+
+#define PIR_MOTION_SENSOR (WIO_D38)
+#define INTERVAL (5000)
+
+WioCellular Wio;
+
+void setup() {
+  SerialUSB.begin(115200);
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---");
+
+  Wio.Init();
+
+  pinMode(PIR_MOTION_SENSOR, INPUT);
+}
+
+void loop() {
+  SerialUSB.println(isPeopleDetected());
+  delay(INTERVAL);
+}
+
+boolean isPeopleDetected() {
+  int sensorValue = digitalRead(PIR_MOTION_SENSOR);
+  SerialUSB.println(sensorValue);
+  if(sensorValue == HIGH) { //if the sensor value is HIGH?
+    return true;//yes,return ture
+  } else {
+    return false;//no,return false
+  }
+}
