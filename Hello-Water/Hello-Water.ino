d8738402e3cc4094e63bd3c0fb47d22b3ed5a593
KazuyukiEguchi <kazuyuki.eguchi@gmail.com>
2019-06-26 10:52:58 +0900

    Update Hello-Water.ino
    2019動作OK

--- Hello-Water/Hello-Water.ino
+++ Hello-Water/Hello-Water.ino
@@ -1,8 +1,11 @@
+// Board ver 1.2.0 / Library 1.7.0 OK 
 // GROVE 水センサを繋いだ場合のサンプルプログラム
 
 #include <WioCellLibforArduino.h>
 
-#define WATER_SENSOR (WIO_D20)
+// #define WATER_SENSOR (WIO_D38)
+// #define WATER_SENSOR (WIO_D20)
+
 #define INTERVAL (1000)
 
 WioCellular Wio;
@@ -13,6 +16,9 @@ void setup() {
   SerialUSB.println("--- START ---");
 
   Wio.Init();
+
+  // GROVE端子へ電源供給を行う(D38以外向け）
+  Wio.PowerSupplyGrove(true);
 
   pinMode(WATER_SENSOR, INPUT);
 }
