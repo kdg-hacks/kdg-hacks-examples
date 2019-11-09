d24d60d9d250f65c49746a0e3fc6a94950ed4b77
Makoto Shinohara <jyoniums@gmail.com>
2019-11-09 15:06:02 +0900

    Water Sensorのサンプル追加

---
+++ m5stack/Hello-WaterSensor/Hello-WaterSensor.ino
@@ -0,0 +1,24 @@
+/**
+ ** Name: Hello-SHT31.ino
+ *
+ * M5Stackで温湿度センサ(SHT31)を使うサンプル
+ * PORT Bにつないで用いる
+ *
+ * 参考: http://wiki.seeedstudio.com/Grove-Water_Sensor/
+ */
+#include <M5Stack.h>
+#define WATER_SENSOR 36
+
+void setup()
+{
+    // Initialize the M5Stack object
+    M5.begin();
+    Serial.begin(9600);
+    pinMode(WATER_SENSOR, INPUT);
+}
+void loop()
+{
+    Serial.println(digitalRead(WATER_SENSOR));
+    M5.Lcd.println(digitalRead(WATER_SENSOR));
+    delay(1000);
+}
